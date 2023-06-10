8d6771edbe79b9ef91c0288ad6502048c137051b
mofe <ogunbiyioladapo33@gmail.com>
2023-06-10 19:47:24 +0100

    updates

--- app/dashboard/expenses/[expenseId]/details/page.js
+++ app/dashboard/expenses/[expenseId]/details/page.js
@@ -1,7 +1,102 @@
+"use client";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+import { useRouter } from "next/navigation";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+const images = [
+  {
+    id: 1,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+    imageAlt: "Back of women's Basic Tee in black.",
+    primary: true,
+  },
+  {
+    id: 2,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+    imageAlt: "Side profile of women's Basic Tee in black.",
+    primary: false,
+  },
+  {
+    id: 3,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
+    imageAlt: "Front of women's Basic Tee in black.",
+    primary: false,
+  },
+];
 export default function ExpenseDetails() {
+  const router = useRouter();
   return (
-    <>
-      <h1>Details</h1>
-    </>
+    <div>
+      <button className="w-7" onClick={() => router.back()}>
+        <ArrowLeftIcon className="text-black" />
+      </button>
+      <div className="px-4 sm:px-0 mt-3">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Expense Details
+        </h3>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Expense name
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Lavish goals
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Expense date
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              2013/01/01
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Expense Category
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              One
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Additional Information
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+              incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+              consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+              proident. Irure nostrud pariatur mollit ad adipisicing
+              reprehenderit deserunt qui eu.
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Supporting Image
+            </dt>
+            <div className="mt-2 grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-3 lg:gap-8">
+              <img
+                src={images[0].imageSrc}
+                alt={images[0].imageAlt}
+                className={classNames(
+                  images[0].primary
+                    ? "lg:col-span-2 lg:row-span-2"
+                    : "hidden lg:block",
+                  "rounded-lg"
+                )}
+              />
+            </div>
+          </div>
+        </dl>
+      </div>
+    </div>
   );
 }
--- app/dashboard/orders/[orderId]/details/page.js
+++ app/dashboard/orders/[orderId]/details/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { PaperClipIcon, ArrowLeftIcon } from "@heroicons/react/20/solid";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
 
 export default function OrderDetails() {
@@ -50,6 +50,14 @@ export default function OrderDetails() {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
+              Order date
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              20/01/2023
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
               Product units sold
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
@@ -66,62 +74,6 @@ export default function OrderDetails() {
               consequat sint. Sit id mollit nulla mollit nostrud in ea officia
               proident. Irure nostrud pariatur mollit ad adipisicing
               reprehenderit deserunt qui eu.
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Attachments
-            </dt>
-            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul
-                role="list"
-                className="divide-y divide-gray-100 rounded-md border border-gray-200"
-              >
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        resume_back_end_developer.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        coverletter_back_end_developer.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
             </dd>
           </div>
         </dl>
